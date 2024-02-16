e16272908e0b3451ddb3d9463ce868f692535a41
vitormrocha <114618745+vitormrocha@users.noreply.github.com>
2024-02-16 18:56:56 -0300

    Update Endereco.java

--- Endereco.java
+++ Endereco.java
@@ -1,12 +1,12 @@
 public class Endereco {
-    private String rua;
-    private int numero;
-    private String complemento;
-    private String cidade;
-    private String estado;
+    private String rua; // verificado
+    private int numero; //verificado
+    private String complemento; //verificado
+    private String cidade; //verificado
+    private String estado; //verificado
+ 
 
-
-    public Endereco(String rua, int numero, String complemento, String cidade, String estado)
+    /* public Endereco(String rua, int numero, String complemento, String cidade, String estado)
     {
         this.rua = rua;
         if (numero > 0)
@@ -14,10 +14,20 @@ public class Endereco {
         this.complemento = complemento;
         this.cidade = cidade;
         this.estado = estado;
+    } */ 
+
+    private boolean verificarRua(String rua) {
+        if (rua.length() == 0 && rua == " ") 
+            return false;
+        else
+            return true;
     }
 
     public void setRua(String rua) {
-        this.rua = rua;
+        if (verificarRua(rua) == true)
+            this.rua = rua;
+        else 
+            System.out.printf("\nERRO! RUA INVALIDA.\n");
     }
 
     public String getRua() {
@@ -35,28 +45,65 @@ public class Endereco {
         return numero;
     }
 
+    private boolean verificarComplemento(String complemento) {
+        if (complemento.length() == 0 || complemento == " ") 
+            return false;
+        else 
+            return true;
+    }
     public void setComplemento(String complemento) {
-        this.complemento = complemento;
+        if (verificarComplemento(complemento) == true)
+            this.complemento = complemento;
+        else 
+            System.out.printf("\nERRO. COMPLEMENTO INVALIDO");
     }
 
     public String getComplemento() {
         return complemento;
     }
 
+    private boolean verificarCidade (String cidade) {
+        if (cidade.length() <= 0 || cidade == " " ) 
+            return false;
+        else 
+            return true;
+    }
+
     public void setCidade(String cidade) {
-        this.cidade = cidade;
+        if (verificarCidade(cidade) == true)
+            this.cidade = cidade;
+        else 
+            System.out.printf("\nERRO. CIDADE INVALIDA.\n");
     }
 
     public String getCidade() {
         return cidade;
     }
 
+    private boolean verificarEstado (String estado) {
+        if (estado.length() <= 0 || estado == " ") 
+            return false;
+        else return true;
+    }
+
     public void setEstado(String estado) {
-        this.estado = estado;
+        if (verificarEstado(estado) == true)
+            this.estado = estado;
+        else 
+            System.out.printf("\nERRO.ESTADO INVALIDO!\n");
     }
 
     public String getEstado() {
         return estado;
+    }
+
+    public Endereco (String rua, int numero, String complemento, String cidade, String estado) {
+        setRua(rua);
+        setNumero(numero);
+        setComplemento(complemento);
+        setCidade(cidade);
+        setEstado(estado);
+
     }
 
     public void exibeEndereco()
