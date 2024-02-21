2d04c49e4e34dcfa8233e48ce623fb0bd6fb4215
vitormrocha <114618745+vitormrocha@users.noreply.github.com>
2024-02-21 20:54:48 -0300

    Update Compra.java

--- Versão 1.3/Compra.java
+++ Versão 1.3/Compra.java
@@ -3,7 +3,21 @@ public class Compra {
     private Hotel hotel;
     private Contabancaria conta;
     private boolean compra_sucesso;
+    private Data data;
+    private Horario horario;
 
+    public Data getData() {
+        return data;
+    }
+    public void setData(Data data) {
+        this.data = data;
+    }
+    public Horario getHorario() {
+        return horario;
+    }
+    public void setHorario(Horario horario) {
+        this.horario = horario;
+    }
     public boolean isCompra_sucesso() {
         return compra_sucesso;
     }
@@ -28,11 +42,14 @@ public class Compra {
     public void setHotel(Hotel hotel) {
         this.hotel = hotel;
     }
-    public Compra (Cliente cliente, Hotel hotel, Contabancaria conta) {
+    public Compra (Cliente cliente, Hotel hotel, Contabancaria conta, Data data, Horario horario) {
         this.cliente = cliente;
         this.hotel = hotel;
         this.conta = conta;
         compra_sucesso = false;
+        this.data = data; 
+        this.horario = horario;
+
     }
     private boolean saldo_suficiente (double preco) {
         if (conta.getSaldo() < preco) 
@@ -40,18 +57,22 @@ public class Compra {
         else 
             return true;
     }
-    public void comprar(int opcao, double preco) {
+    public void comprar(ProdutosOferecidos produto) {
 
-        if (saldo_suficiente(preco)) {
-            conta.sacar(preco);
+        if (saldo_suficiente(produto.getPreco())) {
+            conta.sacar(produto.getPreco());
             int aux = hotel.getQuant_passagens() - 1;
             hotel.setQuant_passagens(aux);
             compra_sucesso = true;
-            cliente.setCodigo_ultcompra(opcao);
+            HistoricoPesquisa hist = new HistoricoPesquisa(this.hotel, produto, this.cliente, this.data, produto.getQuant(), this.horario);
+            cliente.setHistorico(hist);
+            
             
         } else {
             compra_sucesso = false;
         }
+
+        
 
        
     } 
