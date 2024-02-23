9ca2b18cd34e7dc10415194365d4db826e24dfe0
vitormrocha <114618745+vitormrocha@users.noreply.github.com>
2024-02-22 22:02:38 -0300

    Update Cliente.java

--- Versão 1.4/Cliente.java
+++ Versão 1.4/Cliente.java
@@ -41,8 +41,6 @@ public class Cliente {
 
     private int codigo_ultcompra;
 
-    private int quantcompras_vip; //valor de compras que o cliente precisa realizar para se tornar vip
-
     public Contabancaria getContabancaria() {
         return contabancaria;
     }
@@ -81,18 +79,6 @@ public class Cliente {
         return numeroCompras;
     }
 
-    public int getQuantcompras_vip() {
-        return quantcompras_vip;
-    }
-
-
-
-    public void setQuantcompras_vip(int quantcompras_vip) {
-        if (quantcompras_vip < 0)
-            quantcompras_vip = 0;
-        else
-            this.quantcompras_vip = quantcompras_vip;
-    }
 
 
 
@@ -194,7 +180,7 @@ public class Cliente {
         return email;
     }
 
-    public Cliente(String nom, CPF cpf1, Data nasci, Data datacadastro, Endereco endereco, String email, int num, VIP vip, Contabancaria contabancaria, int quantcompras_vip) {
+    public Cliente(String nom, CPF cpf1, Data nasci, Data datacadastro, Endereco endereco, String email, int num, VIP vip, Contabancaria contabancaria) {
         setCpf(cpf1);
         setEmail(email);
         setNome(nom);
@@ -206,7 +192,6 @@ public class Cliente {
         codigo_ultcompra = -1;    
         this.contabancaria = contabancaria;
         this.historico = new HistoricoPesquisa();
-        setQuantcompras_vip(quantcompras_vip);
     }
 
     public void exibeCliente()
@@ -248,10 +233,6 @@ public class Cliente {
 
     public void ExibirHistoricoPesquisa() {
         historico.ExibirHistorico();
-    }
-
-    
-
-    
+    }  
 
 }
