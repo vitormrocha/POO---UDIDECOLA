e86c4a018d7379e1f25d3e185a96e2b18044adbd
vitormrocha <114618745+vitormrocha@users.noreply.github.com>
2024-03-06 19:44:00 -0300

    Update TrechosProcurados.java

--- Versão 2.4/src/TrechosProcurados.java
+++ Versão 2.4/src/TrechosProcurados.java
@@ -1,24 +1,14 @@
 public class TrechosProcurados {
     private int num_pesquisa;
     private String nome_destino;
-    private String destino;
 
-    public TrechosProcurados(int num_pesquisa, String nome, String destino) {
+    public TrechosProcurados(int num_pesquisa, String nome) {
         setNum_pesquisa(num_pesquisa);
         setNome_destino(nome);
-        setDestino(destino);
     }
     
     public int getNum_pesquisa() {
         return num_pesquisa;
-    }
-    public String getDestino() {
-        return destino;
-    }
-
-    public void setDestino(String destino) {
-        if (!destino.isBlank() || !destino.isEmpty())
-            this.destino = destino;
     }
 
     public void setNum_pesquisa(int num_pesquisa) {
